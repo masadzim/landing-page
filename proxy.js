d452457d9d6b432afc0745d5b3b549a376d7c2e9
masadzim <masadzim@gmail.com>
2026-06-22 13:59:13 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -19,7 +19,7 @@
 // ============================================================
 
 // ⚠️ GANTI dengan URL Web App Apps Script kamu (yang berakhiran /exec)
-const GAS_URL = process.env.GAS_URL || 'GANTI_DENGAN_URL_APPS_SCRIPT_EXEC';
+const GAS_URL = process.env.GAS_URL || 'https://script.google.com/macros/s/AKfycbx_p4L2kJFZEciZHpG5cyXDM1wnzB6i0nE5u9p42P08uytzTHBV9etoXp-pgz65NohEkg/exec';
 
 export default async function handler(req, res) {
   // ── CORS Headers ─────────────────────────────────────────
